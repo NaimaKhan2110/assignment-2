e6a63965f7d1ef13c64443b853a377cbcd666756
NaimaKhan2117 <naimakhan2117@gmail.com>
2025-02-11 21:58:34 +0600

    ready for upload

--- event_management/settings.py
+++ event_management/settings.py
@@ -8,7 +8,8 @@ SECRET_KEY = 'your-secret-key'  # Replace with your actual secret key
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRD_TRUSTED_ORIGIN = ['https://*.onrender.com','http://127.0.0.1:8000']
 
 # Application definition
 INSTALLED_APPS = [
